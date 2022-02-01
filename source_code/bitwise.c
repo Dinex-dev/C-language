da04cf477db1591f3838ef8e064e39fe00457fae
Dinesh Thakur <66509841+Dinex-dev@users.noreply.github.com>
2022-02-01 20:38:34 +0530

    removes

--- source_code/bitwise.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int i=4,j;
-	j=i;
-	printf("%d",j);
-	return 0;
-}
--- source_code/demos.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-int main(){
-	printf("Name: Dinesh Thkaur \nCourse: BCA\nCET rank : 116\n");
-}
