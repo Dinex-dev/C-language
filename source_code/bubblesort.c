e05615205a3f335fa15413718998ce83a2423832
Dinesh Thakur <66509841+Dinex-dev@users.noreply.github.com>
2022-02-01 11:10:54 +0530

    bubblesort

--- source_code/bubblesort.c
+++ source_code/bubblesort.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
+int array(int len,int arr[]){
+	for (int i=0;i<len;i++)
+	{
+		printf("%d, ",arr[i]);
+	}
+	printf("\n");
+}
 int main()
 {
 	int len,spam;
-	printf("Enter the Length of array : ");
+	printf("Enter length of the array : ");
 	scanf("%d",&len);
 	int arr[len];
-	for (int i = 1; i <= len; i++)
-	{
-		printf("Enter %d element : ",i);
+	for (int i=0;i<len;i++){
+		printf("Enter %d value : ",i);
 		scanf("%d",&arr[i]);
 	}
-	for (int i = 1; i <= len; i++)
-	{
-		printf("%d ,",arr[i]);
-	}
-	printf("\n");
-	for (int i = 0; i < len; i++)
-	{
-		for (int j = 0; j < len-i; j++)
+	for (int i=0;i<len-1;i++)
+	{	
+		for (int j=0;j<len-1;j++)
 		{
-			
 			if (arr[j]>arr[j+1])
 			{
+				array(len,arr);
 				spam=arr[j+1];
 				arr[j+1]=arr[j];
 				arr[j]=spam;
 			}
-			
 		}
-		
+		printf("\n");
 	}
-	for (int i = 1; i <= len; i++)
+	for (int i=0;i<len;i++)
 	{
-		printf("%d ,",arr[i]);
+		printf("%d, ",arr[i]);
 	}
 	return 0;
 }
