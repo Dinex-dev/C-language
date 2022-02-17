9d31113da7f2d7e1c0ab58bc5e473a5d4405b0a1
Dinesh Thakur <66509841+Dinex-dev@users.noreply.github.com>
2022-02-17 14:24:04 +0530

    calculator using pointers

---
+++ source_code/calculator_using_pointers.c
@@ -0,0 +1,17 @@
+// write a programm to add, substrac, multiply and divide two numbers using pointers
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    int *pa, *pb;
+    pa = &a;
+    pb = &b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", pa, pb);
+    printf("\nAddition: %d", *pa + *pb);
+    printf("\nSubstraction: %d", *pa - *pb);
+    printf("\nMultiplication: %d", *pa * *pb);
+    printf("\nDivision: %d", *pa / *pb);
+    return 0;
+}
+                    
